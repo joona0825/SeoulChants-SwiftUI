08a5b12e44e6a3ad453332477320dc82ec2b858c
alfred <alfred@honestfund.kr>
2021-01-27 23:36:35 +0900

    Initial Commit

---
+++ Shared/SeoulChants_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeoulChants_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Alfred Woo on 2021/01/27.
+//
+
+import SwiftUI
+
+@main
+struct SeoulChants_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
